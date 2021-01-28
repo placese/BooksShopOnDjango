55c3dc863bafe3c90aeefaca0c363c8dab4a60dd
placese <thegunter@yandex.ru>
2021-01-28 16:26:09 +0300

    start

--- books/store/views.py
+++ books/store/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .serializers import BookSerializer
+from .models import Book
+
+
+class BookViewSet(ModelViewSet):
+	queryset = Book.objects.all()
+	serializer_class = BookSerializer
