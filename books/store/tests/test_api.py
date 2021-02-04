700fadfb72707535f7fdfec7d1364647399de538
placese <thegunter@yandex.ru>
2021-02-04 14:47:07 +0300

    added permissions, new unit tests

--- books/store/tests/test_api.py
+++ books/store/tests/test_api.py
@@ -1,5 +1,6 @@
 from rest_framework.test import APITestCase
 from rest_framework import status
+from rest_framework.exceptions import ErrorDetail
 
 from django.urls import reverse
 
@@ -7,7 +8,6 @@ from django.contrib.auth.models import User
 
 from store.models import Book
 from store.serializers import BookSerializer
-# from store.views import BookViewSet
 
 import json
 
@@ -15,7 +15,8 @@ import json
 class BooksApiTestCase(APITestCase):
 	def setUp(self):
 		self.user = User.objects.create(username='test_username')
-		self.book_1 = Book.objects.create(name='Test book 1', price=1000.00, author_name='Author 1')
+		self.user2 = User.objects.create(username='test_username2')
+		self.book_1 = Book.objects.create(name='Test book 1', price=1000.00, author_name='Author 1', owner=self.user)
 		self.book_2 = Book.objects.create(name='Test book 2', price=1250.00, author_name='Author 2')
 		self.book_3 = Book.objects.create(name='Test book Author 1', price=700.00, author_name='Author 3')
 
@@ -62,6 +63,7 @@ class BooksApiTestCase(APITestCase):
 		response = self.client.post(url, data=json_data, content_type='application/json')
 		self.assertEqual(status.HTTP_201_CREATED, response.status_code)
 		self.assertEqual(4, Book.objects.all().count())
+		self.assertEqual(self.user, Book.objects.last().owner)
 
 	def test_update(self):
 		url = reverse('book-detail', args=(self.book_1.id,))
@@ -78,9 +80,57 @@ class BooksApiTestCase(APITestCase):
 		self.book_1.refresh_from_db()
 		self.assertEqual(2000, self.book_1.price)
 
+	def test_update_not_owner(self):
+		url = reverse('book-detail', args=(self.book_1.id,))
+		data = {
+				"name": self.book_1.name,
+				"price": 2000.00,
+				"author_name": self.book_1.author_name
+		}
+		json_data = json.dumps(data)
+		self.client.force_login(self.user2)
+
+		response = self.client.put(url, data=json_data, content_type='application/json')
+		self.assertEqual(status.HTTP_403_FORBIDDEN, response.status_code)
+		self.assertEqual(
+			{'detail': ErrorDetail(string='You do not have permission to perform this action.', 
+								code='permission_denied')}, 
+								response.data
+			)
+		self.book_1.refresh_from_db()
+		self.assertEqual(1000, self.book_1.price)
+
+	def test_update_not_owner_but_staff(self):
+		self.user3 = User.objects.create(username='test_username3', is_staff=True)
+		url = reverse('book-detail', args=(self.book_1.id,))
+		data = {
+				"name": self.book_1.name,
+				"price": 2000.00,
+				"author_name": self.book_1.author_name
+		}
+		json_data = json.dumps(data)
+		self.client.force_login(self.user3)
+
+		response = self.client.put(url, data=json_data, content_type='application/json')
+		self.assertEqual(status.HTTP_200_OK, response.status_code)
+		self.book_1.refresh_from_db()
+		self.assertEqual(2000, self.book_1.price)
+
 	def test_delete(self):
 		url = reverse('book-detail', args=(self.book_1.id,))
 		self.client.force_login(self.user)
 
 		response = self.client.delete(url)
 		self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
+
+	def test_delete_not_owner(self):
+		url = reverse('book-detail', args=(self.book_1.id,))
+		self.client.force_login(self.user2)
+
+		response = self.client.delete(url)
+		self.assertEqual(status.HTTP_403_FORBIDDEN, response.status_code)
+		self.assertEqual(
+			{'detail': ErrorDetail(string='You do not have permission to perform this action.', 
+								code='permission_denied')}, 
+								response.data
+			)
--- books/store/views.py
+++ books/store/views.py
@@ -1,21 +1,26 @@
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+# from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django.shortcuts import render
 
 from .serializers import BookSerializer
 from .models import Book
+from .permissions import IsOwnerOrStaffOrReadOnly
 
 
 class BookViewSet(ModelViewSet):
 	queryset = Book.objects.all()
 	serializer_class = BookSerializer
 	filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-	permission_classes = [IsAuthenticatedOrReadOnly]
+	permission_classes = [IsOwnerOrStaffOrReadOnly]
 	filter_fields = ['price']
 	search_fields = ['name', 'author_name']
 	ordering_fields = ['price', 'author_name']
+
+	def perform_create(self, serializer):
+		serializer.validated_data['owner'] = self.request.user
+		serializer.save()
 
 
 def auth(request):
